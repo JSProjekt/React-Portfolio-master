335480562aae0e0495e60b0c42c6560410ec2d79
Jordan S <168541998+JSProjekt@users.noreply.github.com>
2024-11-30 10:45:11 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,146 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Sicre Jordan",
+    "role": "Developeur web Frontend Javascript/React",
+    "linkedinId":" jordan.sicre@email.com",
+    "skypeid": "",
+    "roleDescription": "J'aime m'impliquer dans diverses parties du développement frontend et j'aime en apprendre davantage sur les nouvelles technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jordansicre",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/JSProjekt",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/orkhitech/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"Je suis actuellement en fin de formation de développeur web Frontend Javascript/React chez OpenClassRooms. J'ai travaillé sur des projets de formation et des projets personnels. J'ai une passion pour le développement. J'aime apprendre de nouvelles technologies et améliorer mes compétences en programmation.",
+    "address":"France",
+    "website":"https://github.com/JSProjekt",
+    "education":[
+      {
+        "UniversityName":"OpenclassRooms",
+        "specialization":"Développeur Frontend web et web mobile Javascript/React",
+        "MonthOfPassing":"Décembre",
+        "YearOfPassing":"2024",
+        
+      },
+      {
+        "UniversityName":"Lycée professionnel Pierre-andré Chabanne",
+        "specialization":"Bac professionnel Systèmes Numériques",
+        "MonthOfPassing":"Juillet",
+        "YearOfPassing":"2007",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Terreal",
+        "specialization":"Technicien informatique",
+        "MonthOfLeaving":"Aout",
+        "YearOfLeaving":"2009",
+      }
+    ],
+    "skillsDescription":"J'ai des compétences pour le developpement web et en Maintenance informatique",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Linux-Windows"
+      },
+      {
+        "skillname":"Hardware"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Riding Cities",
+        "description":"mobileapp",
+        "imgurl":"https://github.com/JSProjekt/Projet_2_Sicre_Jordan/blob/main/images/header.png?raw=true",
+        "preview":"https://jsprojekt.github.io/Projet_2_Sicre_Jordan/",
+        "icons":["fa-html5"]
+      },
+      {
+        "name":"Booki",
+        "description":" À partir d'une maquette Figma, j'ai développé une landing page et implémenté les media queries pour la compatibilité avec les versions tablette et mobile.",
+        "imgurl":"https://github.com/JSProjekt/Projet_3_Sicre_Jordan/blob/main/images/logo/Booki@3x.png?raw=true",
+        "preview":"https://jsprojekt.github.io/Projet_3_Sicre_Jordan/",
+        "icons":["fa-html5","fa-css3"]},
+      {
+        "name":"Ohmyfood",
+        "description":"À partir d'une maquette Figma et l'utilisation du concept mobile first, j'ai créé une page d'accueil, plusieurs pages de menus et intégré un élément de chargement.",
+        "preview":"https://jsprojekt.github.io/Openclassrooms_P4_ohmyfood/",
+        "imgurl":"https://github.com/JSProjekt/Openclassrooms_P4_ohmyfood/blob/main/16559016787093_Untitled%20design.png?raw=true",
+        "icons":["fa-html5","fa-css3","fa-sass"]
+      },
+      {
+        "name":"Print it!",
+        "description":"Ici j'ai fait l'intégration d'un carrousel sur un site.",
+        "preview":"https://jsprojekt.github.io/Openclassrooms_P5_Print_it/",
+        "imgurl":"https://user.oc-static.com/upload/2022/06/17/16554564334118_FR_1148_P6_Banner-Printit.png",
+        "icons":["fa-js"]
+
+      },
+      {
+        "name":"Sophie Bluel",
+        "description":"En partant d'une maquette conçue sur Figma, j'ai développé une page de connexion et mis en place l'affichage dynamique des travaux dans le portfolio via une API. J'ai intégré une modale permettant à l'administrateur du site d'ajouter ou de supprimer des travaux, conformément aux exigences du projet.",  
+        "imgurl":"https://github.com/JSProjekt/Openclassrooms_P6_SophieBluel/blob/main/FrontEnd/assets/images/sophie-bluel.png?raw=true",
+        "icons":["fa-html5","fa-css3","fa-sass","fa-js"]
+      },
+      {
+        "name":"Nina Carducci",
+        "description":"Ma mission était d'optimiser le site web, d'améliorer son référencement SEO, et de générer un rapport sur les optimisations effectuées.",
+        "imgurl":"images/portfolio/Nina.jpg",
+        "icons":["fa-"]
+      },
+      {
+        "name":"Kasa",
+        "description":"J'ai effectué la refonte d'un site d'agence immobilière. Avec l'aide d'une maquette Figma et d'un fichier JSON pour intégrer les données sur le site.",
+        "imgurl":"images/portfolio/Nina.jpg",
+        "icons":["fa-react","fa-sass"]
+      },
+      {
+        "name":"ArgentBank",
+        "description":"J'ai du réaliser l'intégration du système d'authentification avec Redux Toolkit et établir un mécanisme de transaction conforme avec un Swagger.",
+        "imgurl":"images/portfolio/Nina.jpg",
+        "icons":["fa-react"]
+      }
+    
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
